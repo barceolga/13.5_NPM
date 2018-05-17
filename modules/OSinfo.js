173294f12e642029d974d75dce11cbccd27550ca
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-17 10:47:00 +0200

    Add modules folder and OSinfo.js and timeCalculator.js files

---
+++ modules/OSinfo.js
@@ -0,0 +1,28 @@
+
+var timeInfo = require('../modules/timeCalculator')
+var os = require('os');
+var  colors = require('colors');
+
+function getOSinfo() {
+  var type = os.type();
+  if (type === 'Darwin') {
+    type = 'OSX';
+  }else if (type === 'Windows_NT') {
+    type = 'Windows';
+  }
+  var release = os.release();
+  var cpu = os.cpus()[0].model;
+  var uptime = os.uptime();
+  var userInfo = os.userInfo();
+
+
+  console.log(colors.blue.bold('System:'), type);
+  console.log(colors.bgMagenta.red('Release:'), release);
+  console.log(colors.green.bold('CPU model:'), cpu);
+  console.log(colors.bgYellow.red('Uptime:'), timeInfo.print(uptime));
+  console.log(colors.cyan.bold('User name:'), userInfo.username);
+  console.log(colors.bgRed.yellow('Home dir:'), userInfo.homedir);
+}
+
+
+exports.print = getOSinfo;
