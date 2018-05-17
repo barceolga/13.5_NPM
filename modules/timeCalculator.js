dc63b44588486fc7b6e4edd8356412430c33ec75
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-17 11:00:11 +0200

    Update content for timeCalculator.js

--- modules/timeCalculator.js
+++ modules/timeCalculator.js
@@ -2,11 +2,11 @@
 
 function timeCalculator(time) {
   if ( time < 60) {
-    console.log( time, "sec");
+    console.log( time, 'sec');
   } else if ( (time >= 60) && (time < 3600 )) {
-    console.log( Math.floor(time / 60), "min", (time % 60), "sec");
+    console.log( Math.floor(time / 60), 'min', (time % 60), 'sec');
   } else if (time >= 3600) {
-    console.log( Math.floor(time / 3600), "hour(s)", (Math.floor((time % 3600)/60)), "min", (time % 60), "sec");
+    console.log( Math.floor(time / 3600), 'hour(s)', (Math.floor((time % 3600)/60)), 'min', (time % 60), 'sec');
   }
 }
 
